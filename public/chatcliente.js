fa601ab5b052a84f84a8abc276e21c29447112eb
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-08 12:10:28 +0000

    ok

--- public/chatcliente.js
+++ public/chatcliente.js
@@ -264,7 +264,35 @@ async function pagarComCartao() {
 
   const { clientSecret } = await res.json();
 
-  elements = stripe.elements({ clientSecret });
+  elements = stripe.elements({
+  clientSecret,
+  appearance: {
+    theme: "flat",
+    variables: {
+      colorPrimary: "#7B2CFF",        // 🔥 roxo Velvet
+      colorBackground: "#ffffff",
+      colorText: "#4a2bbf",
+      colorDanger: "#d9534f",
+
+      fontFamily: "Poppins, system-ui, sans-serif",
+      fontSizeBase: "14px",
+      borderRadius: "12px"
+    },
+    rules: {
+      ".Input": {
+        border: "1px solid #9E6BFF"
+      },
+      ".Input:focus": {
+        borderColor: "#7B2CFF",
+        boxShadow: "0 0 0 2px rgba(123,44,255,0.25)"
+      },
+      ".Label": {
+        color: "#7B2CFF",
+        fontWeight: "500"
+      }
+    }
+  }
+});
   const paymentElement = elements.create("payment");
   paymentElement.mount("#payment-element");
 }
