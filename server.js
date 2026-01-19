023dbe7da14df4110694528bcde1bef21347a690
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-19 17:11:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -288,7 +288,6 @@ const authLimiter = rateLimit({
 });
 
 const servercontent = require("./servercontent");
-app.use("/", servercontent);
 
 const requireRole = require("./middleware/requireRole");
 // ===============================
@@ -2621,6 +2620,10 @@ process.on("uncaughtException", err => {
   console.error("❌ Uncaught Exception:", err);
 });
 
+
+
+
+app.use("/", servercontent);
 
 // ===============================
 // START SERVER
