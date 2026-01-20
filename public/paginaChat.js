c414e6fa2c36ea1f9990d183cc687dcba363e525
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-20 13:59:03 +0000

    ,

--- public/paginaChat.js
+++ public/paginaChat.js
@@ -82,22 +82,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       enviarMensagem();
     }
   });
-
-  await carregarModelo();
-  await carregarCliente();
-  await carregarConteudosVistos(cliente_id);
-
-  // 🔙 BOTÃO VOLTAR
-  const btnVoltar = document.getElementById("btnVoltar");
-  btnVoltar.onclick = voltar;
-
-  // ✉️ BOTÃO ENVIAR
-  const btnEnviar = document.getElementById("chatEnviar");
-  btnEnviar.onclick = enviarMensagem;
-
-  
 });
-
 
 // ===============================
 // FUNÇÕES
