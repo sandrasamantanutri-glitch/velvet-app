8e96476d71019326b6aa72c51fb1bfbd03cb6a04
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-20 12:29:33 +0000

    Revert "."
    This reverts commit ae20135ccdd97d103a2fa19a6e60326b1c18d53b.

--- public/profile.js
+++ public/profile.js
@@ -11,19 +11,6 @@ const socket = io();
 const params = new URLSearchParams(window.location.search);
 const modeloParam = params.get("id");
 
-const token = localStorage.getItem("token");
-const role  = localStorage.getItem("role");
-
-console.group("🛡️ DEBUG PERFIL");
-console.log("URL:", window.location.href);
-console.log("modeloParam (?id):", modeloParam);
-console.log("token existe?", !!token);
-console.log("role:", role);
-console.log("modelo_id LS:", localStorage.getItem("modelo_id"));
-console.groupEnd();
-
-
-
 //DEFINIÇÃO SEGURA DE MODO
 let modo = "publico";
 if (token && role === "modelo" && !modeloParam) {
@@ -60,6 +47,8 @@ function decodeJWT(token) {
     return null;
   }
 }
+
+
 
 function logout() {
   localStorage.clear();
