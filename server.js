1f4c50b8f9b468679e1266797393bfb0a5ac76c2
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-21 17:44:26 +0000

    .

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const nodemailer = require("nodemailer");
 app.use((req, res, next) => {
   console.log("➡️ REQ:", req.method, req.url);
   next();
