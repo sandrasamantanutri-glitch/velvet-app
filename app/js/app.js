7d280fe259936607ae93a1bfb730c7da96682c77
Velvet <sandrasamanta.nutri@gmail.com>
2026-01-23 18:30:51 +0000

    .

--- app/js/app.js
+++ app/js/app.js
@@ -13,18 +13,20 @@ function abrirInbox() {
 function abrirChat() {
   document.getElementById("view-chat").innerHTML = `
     <div style="padding:16px">
-      <p>Chat aberto com sucesso 💬</p>
+      <p><strong>Chat aberto 💬</strong></p>
+      <p>A barra de endereço NÃO voltou 🎉</p>
     </div>
   `;
 
   document.getElementById("view-chat").classList.add("active");
   document.getElementById("view-inbox").classList.remove("active");
-  document.getElementById("topbar-title").innerText = "Chat";
+  document.getElementById("topbar-title").innerText = "Modelo Teste";
 }
+
 
 function voltarInbox() {
   abrirInbox();
 }
 
-// inicia
+
 abrirInbox();
