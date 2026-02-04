45bbfce2fbfe1428cd9cae510c91e0d1792d7e35
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-02-04 09:32:02 +0000

    .

--- public/perfil.js
+++ public/perfil.js
@@ -1,7 +1,7 @@
 // ===============================
 // AUTH GUARD
 // ===============================
-const stripe = Stripe("pk_live_51Spb5lRtYLPrY4c3L6pxRlmkDK6E0OSU93T5B75V4pY39rJ3FVyPEa6ZDDgqUiY1XCCEay6uQcItbZY4EcAOkoJn00TtsQ8bbz");
+const stripe = stripe("pk_live_51Spb5lRtYLPrY4c3L6pxRlmkDK6E0OSU93T5B75V4pY39rJ3FVyPEa6ZDDgqUiY1XCCEay6uQcItbZY4EcAOkoJn00TtsQ8bbz");
 let elements;
 window.__CLIENTE_VIP__ = false;
 
@@ -586,6 +586,7 @@ async function excluirMidia(id, card) {
 // ===============================
 
 function aplicarPerfilNoDOM(modelo) {
+  console.log("🔥 PERFIL APLICADO", modelo);
   document.getElementById("perfil-nome").textContent =
     modelo.nome || "";
 
