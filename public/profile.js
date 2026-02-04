9f920c6446434321c39fed667f7bf4b86bd06267
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-02-04 10:57:40 +0000

    .

--- public/profile.js
+++ public/profile.js
@@ -2,7 +2,7 @@
 // AUTH GUARD
 // ===============================
 
-const stripe = Stripe("pk_live_51Spb5lRtYLPrY4c3L6pxRlmkDK6E0OSU93T5B75V4pY39rJ3FVyPEa6ZDDgqUiY1XCCEay6uQcItbZY4EcAOkoJn00TtsQ8bbz");
+// const stripe = Stripe("pk_live_51Spb5lRtYLPrY4c3L6pxRlmkDK6E0OSU93T5B75V4pY39rJ3FVyPEa6ZDDgqUiY1XCCEay6uQcItbZY4EcAOkoJn00TtsQ8bbz");
 let elements;
 window.__CLIENTE_VIP__ = false;
 
