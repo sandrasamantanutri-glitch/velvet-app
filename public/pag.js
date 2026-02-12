41347b628f49d0add1bfe8342de67f5fcf7da342
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-02-12 12:40:18 +0000

    .

--- public/pag.js
+++ public/pag.js
@@ -653,8 +653,8 @@ window.iniciarCartaoMidia = async function () {
       Authorization: "Bearer " + token
     },
     body: JSON.stringify({
-       message_id: window.MIDIA_VENDA_ATUAL?.message_id
-    })
+   conteudo_id: window.MIDIA_VENDA_ATUAL?.conteudo_id
+})
   });
 
   if (!res.ok) {
