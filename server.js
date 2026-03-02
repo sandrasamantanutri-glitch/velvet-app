167545822ee8562975d6054507404f7396408fda
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-03-02 17:26:03 +0000

    .

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 //   );
 // });
 
-const { enviarEmailValidacao } = require("/email");
+const { enviarEmailValidacao } = require("./email");
 const os = require("os");
 const { exec } = require("child_process");
 const ffmpeg = require("fluent-ffmpeg");
