5beebc618611e2c477b34c4fb3ac30d267d23703
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-03-10 14:02:24 +0000

    Revert "."
    This reverts commit dac2a6606e3073d40a8ab1a91e686149d04a2122.

--- public/perfilnew.js
+++ public/perfilnew.js
@@ -176,7 +176,7 @@ document.getElementById("btnEnviarPremium")
   }
 
  const form = new FormData();
-
+ 
 const files = filePremium.files;
 for(const file of files){
   form.append("file", file);
@@ -240,11 +240,6 @@ tabs.forEach(tab => {
 
 });
 
-document.getElementById("premiumClose")
-?.addEventListener("click", fecharPopupPremium);
-
-document.getElementById("premiumBackdrop")
-?.addEventListener("click", fecharPopupPremium);
 
 
 });
@@ -969,19 +964,6 @@ container.appendChild(card);
   }catch(err){
     console.error("Erro carregar premium:", err);
   }
-
-}
-
-function fecharPopupPremium(){
-
-  const popup = document.getElementById("popupUploadPremium");
-
-  popup.classList.add("hidden");
-
-  filePremium.value = "";
-  previewPremium.innerHTML = "";
-  document.getElementById("premiumTexto").value = "";
-  document.getElementById("premiumPreco").value = "";
 
 }
 
