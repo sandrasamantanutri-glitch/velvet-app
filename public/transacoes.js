613a1a0af2caa6f81064584f9364aeb2cb1330c1
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-03-12 19:13:18 +0000

    .

--- public/transacoes.js
+++ public/transacoes.js
@@ -66,8 +66,7 @@ async function carregarTransacoes() {
   if (!res.ok) {
     lista.innerHTML = `
   <div class="erro-transacoes">
-    Erro ao carregar transações.<br>
-    No momento, apenas clientes podem processar pagamentos.
+    Não existem transações.<br>
   </div>
 `;
     return;
