3e5dbdfd1f92f12933daf3899e4e2f09033eb246
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-09-16 17:21:28 +0100

    correcoes app, index e me

--- android/app/src/main/java/velvet/app/MainActivity.java
+++ android/app/src/main/java/velvet/app/MainActivity.java
@@ -1,11 +1,28 @@
 package velvet.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View webView = getBridge().getWebView();
+            WindowInsetsCompat insets = ViewCompat.getRootWindowInsets(getWindow().getDecorView());
+            if (insets != null) {
+                int topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+                if (topInset > 0 && webView.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+                    ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) webView.getLayoutParams();
+                    params.topMargin = topInset;
+                    webView.setLayoutParams(params);
+                    getWindow().getDecorView().setBackgroundColor(0xFF000000);
+                }
+            }
+        }
     }
 }
