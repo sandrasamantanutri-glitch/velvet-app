505e3ebd7525fc4334d6af6cdb0c4e39fd76634e
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-09-16 16:31:59 +0100

    .

--- android/app/src/main/java/velvet/app/MainActivity.java
+++ android/app/src/main/java/velvet/app/MainActivity.java
@@ -1,13 +1,11 @@
 package velvet.app;
 
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
     }
 }
